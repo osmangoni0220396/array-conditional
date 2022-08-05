4165cf3d179f194ef1dd6730072316ed5ef6493c
Md Osman Goni <osmangoni0220396@gmail.com>
2022-08-06 03:49:23 +0600

    End For tonight

---
+++ conditional.js
@@ -0,0 +1,15 @@
+/*
+    ---- Comparison Operator--- 
+    <   Less than
+    >   Greater Than
+    <=  Less than equal
+    >=  Greater than equal
+    ==  equal
+    !=  equal not
+    === equal value and equal type
+    !== equal not and not equal type
+    &&  jodi duita condition ei sign dara thake er mane duita sortoi puron korte hobe
+    ||  jodi duita condition ei sign dara thake er mane duitar ekta sorto puron korlei hobe. duita sorto o puron korbe.
+*/ 
+
+
